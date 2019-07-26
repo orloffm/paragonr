4f8f7338c9e68b479b69e629b57dbfef5b8501c8
Mikhail Orlov <orloffm@gmail.com>
2019-07-26 21:24:06 +0200

    categories and domain stubs

--- src/backend/Paragonr.Persistence/BudgetDbInitializer.cs
+++ src/backend/Paragonr.Persistence/BudgetDbInitializer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Paragonr.Entities;
 
@@ -17,18 +18,18 @@ namespace Paragonr.Persistence
         {
             context.Database.EnsureCreated();
 
-            if (context.Currencies.Any())
-            {
-                return;
-            }
-
             AddCurrencies(context);
 
             AddDefaultCurrencyRates(context);
+
+            AddDomainsAndCategories(context);
         }
 
         private void AddCurrencies(BudgetDbContext context)
         {
+            if (context.Currencies.Any())
+                return;
+
             Currency[] currencies =
             {
                 new Currency
@@ -64,6 +65,9 @@ namespace Paragonr.Persistence
 
         private void AddDefaultCurrencyRates(BudgetDbContext context)
         {
+            if (context.CurrencyRates.Any())
+                return;
+
             Dictionary<string, long> currencyIds = context.Currencies.ToDictionary(c => c.IsoCode, c => c.Id);
             var date = new DateTime(2019, 7, 14);
 
@@ -87,6 +91,91 @@ namespace Paragonr.Persistence
 
             context.CurrencyRates.AddRange(currencyRates);
 
+            context.SaveChanges();
+        }
+
+        [SuppressMessage("ReSharper", "StringLiteralTypo")]
+        private void AddDomainsAndCategories(BudgetDbContext context)
+        {
+            var domainsAndCategories = new Dictionary<string, string[]>
+            {
+                {
+                    "Дети", new[]
+                    {
+                        "Одежда",
+                        "Еда",
+                        "Подгузники",
+                        "Развлечения"
+                    }
+                },
+                {
+                    "Еда", new[]
+                    {
+                        "Рестораны",
+                        "Снэки"
+                    }
+                },
+                {
+                    "Жизнь", new[]
+                    {
+                        "Квартира",
+                        "Медицина",
+                        "Путешествия",
+                        "Развлечения",
+                        "Сервисы",
+                        "Страховка",
+                        "Транспорт",
+                        "Одежда",
+                        "Чтение"
+                    }
+                },
+                {
+                    "Коты", new string [0]
+                },
+                {
+                    "Машина", new[]
+                    {
+                        "Бензин",
+                        "Парковка",
+                        "Обслуживание"
+                    }
+                }
+            };
+
+            if (context.Domains.Any() == false)
+            {
+                context.Domains.AddRange(domainsAndCategories.Select(kvp => new Domain(){Name = kvp.Key}));
+                context.SaveChanges();
+            }
+
+            if (context.Categories.Any())
+                return;
+
+            var categories = new List<Category>();
+            foreach (KeyValuePair<string, string[]> domainAndCategories in domainsAndCategories)
+            {
+                Domain d = context.Domains.Single(e => e.Name == domainAndCategories.Key);
+
+                var defaultCategory = new Category
+                {
+                    Domain = d,
+                    Name = "-"
+                };
+                d.DefaultCategory = defaultCategory;
+                categories.Add(defaultCategory);
+
+                foreach (string category in domainAndCategories.Value)
+                {
+                    var c = new Category
+                    {
+                        Domain = d,
+                        Name = category
+                    };
+                    categories.Add(c);
+                }
+            }
+
+            context.Categories.AddRange(categories);
             context.SaveChanges();
         }
     }
--- src/frontend/src/data/info-generic.tsx
+++ src/frontend/src/data/info-generic.tsx
@@ -1,12 +1,42 @@
 const currenciesData = {
-  categories: [],
+  categories: [
+    { domainName: "Дети", name: "-" },
+    { domainName: "Машина", name: "Бензин" },
+    { domainName: "Машина", name: "-" },
+    { domainName: "Коты", name: "-" },
+    { domainName: "Жизнь", name: "Чтение" },
+    { domainName: "Жизнь", name: "Одежда" },
+    { domainName: "Жизнь", name: "Транспорт" },
+    { domainName: "Жизнь", name: "Страховка" },
+    { domainName: "Жизнь", name: "Сервисы" },
+    { domainName: "Жизнь", name: "Развлечения" },
+    { domainName: "Машина", name: "Парковка" },
+    { domainName: "Жизнь", name: "Путешествия" },
+    { domainName: "Жизнь", name: "Квартира" },
+    { domainName: "Жизнь", name: "-" },
+    { domainName: "Еда", name: "Снэки" },
+    { domainName: "Еда", name: "Рестораны" },
+    { domainName: "Еда", name: "-" },
+    { domainName: "Дети", name: "Развлечения" },
+    { domainName: "Дети", name: "Подгузники" },
+    { domainName: "Дети", name: "Еда" },
+    { domainName: "Дети", name: "Одежда" },
+    { domainName: "Жизнь", name: "Медицина" },
+    { domainName: "Машина", name: "Обслуживание" }
+  ],
   currencies: [
     { isoCode: "PLN", name: "Polish złoty", symbol: "zł" },
     { isoCode: "USD", name: "US dollar", symbol: "$" },
     { isoCode: "EUR", name: "Euro", symbol: "€" },
     { isoCode: "RUB", name: "Russian ruble", symbol: "₽" }
   ],
-  domains: [],
+  domains: [
+    { defaultCategoryName: "-", name: "Дети" },
+    { defaultCategoryName: "-", name: "Еда" },
+    { defaultCategoryName: "-", name: "Жизнь" },
+    { defaultCategoryName: "-", name: "Коты" },
+    { defaultCategoryName: "-", name: "Машина" }
+  ],
   ratesInfo: {
     defaultCurrency: "PLN",
     rates: [
