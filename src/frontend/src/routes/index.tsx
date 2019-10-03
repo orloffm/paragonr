9642ac4fd7d7bc1aae36d22bd03c99f5cc0801a5
Mikhail Orlov <orloffm@gmail.com>
2019-10-03 23:23:16 +0200

    private route

--- src/frontend/src/routes/index.tsx
+++ src/frontend/src/routes/index.tsx
@@ -8,13 +8,14 @@ import { Categories } from "../pages/Categories";
 import { Spendings } from "../pages/Spendings";
 import { NotFound } from "../pages/NotFound";
 import { Login } from "../pages/Login";
+import { PrivateRoute } from "../components/PrivateRoute";
 
 const routes = (
   <Switch>
     <Route path="/login" component={Login} />
     <Route path="/currencies" component={Currencies} />
     <Route path="/categories" component={Categories} />
-    <Route path="/spendings" component={Spendings} />
+    <PrivateRoute path="/spendings" component={Spendings} isLoggedIn={false}/>
     <Route path="/not-found" component={NotFound} />
     <Route path="/" exact component={Home} />
     <Redirect to="/not-found" />
